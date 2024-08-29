ac4f76bffc00cdbee4b18c70189191cec393e74b
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-29 23:58:15 +0900

    Invoked Spring Boot Hello World Bean & Path Param REST API from React

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -9,12 +9,12 @@ public class HelloWorldController {
 	
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World"; 
+		return "Hello World v2"; 
 	}
 	
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World"); 
+		return new HelloWorldBean("Hello World Bean v1"); 
 	}
 	
 	@GetMapping(path = "/hello-world/path-variable/{name}")
