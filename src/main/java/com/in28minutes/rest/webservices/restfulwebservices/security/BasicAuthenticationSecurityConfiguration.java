d4a7444094b6c15eb54f1737752ce3d91285edcd
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-02 01:11:33 +0900

    Secured Spring Boot REST API with Spring Security

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/security/BasicAuthenticationSecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.in28minutes.rest.webservices.restfulwebservices.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthenticationSecurityConfiguration {
+	
+	
+	// Filter Chain
+	// authenticate all requests
+	// basic authentication
+	// disabling CSRF
+	// STATELESS REST API
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return
+				http.authorizeHttpRequests(
+				auth -> auth.anyRequest().authenticated())
+				.httpBasic(Customizer.withDefaults())
+				.sessionManagement(
+					session -> session.sessionCreationPolicy(
+						SessionCreationPolicy.STATELESS
+					)
+				)
+				.csrf().disable()
+				.build();
+		
+	}
+
+}
