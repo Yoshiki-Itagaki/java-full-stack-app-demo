373ecfdce66b3c8e76c82dc7b3dcddbd093420df
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-02 00:00:29 +0900

    Added Update Todo and CreateTodo REST API to Spring Boot Backend API

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -6,6 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,5 +35,17 @@ public class TodoResource {
 		todoService.deleteById(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@PutMapping("/users/{username}/todos/{id}")
+	public Todo  updateTodo(@PathVariable String username, @PathVariable int id, @RequestBody Todo todo) {
+		todoService.updateTodo(todo);
+		return todo;
+	}
 
+	@PostMapping("/users/{username}/todos")
+	public Todo  createTodo(@PathVariable String username, @RequestBody Todo todo) {
+		Todo createdTodo = todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), todo.isDone());
+		return createdTodo;
+	}
+	
 }
