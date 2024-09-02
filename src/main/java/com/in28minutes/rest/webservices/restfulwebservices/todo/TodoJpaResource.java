ce19f7206537a6cc1a81ebde0b4159e948c3d70d
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-02 21:32:11 +0900

    Full Stack React & Spring Boot with JPA & Hibernate - Todo CRUD

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoJpaResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoJpaResource.java
@@ -11,34 +11,42 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-public class TodoResource {
+import com.in28minutes.rest.webservices.restfulwebservices.todo.repository.TodoRepository;
+
+@RestController
+public class TodoJpaResource {
 	
 	private TodoService todoService;
+	private TodoRepository todoRepository;
 	
-	public TodoResource(TodoService todoService) {
+	public TodoJpaResource(TodoService todoService, TodoRepository todoRepository) {
 		this.todoService = todoService;
+		this.todoRepository = todoRepository;
 	}
 
 	@GetMapping("/users/{username}/todos")
 	public List<Todo> retrieveTodos(@PathVariable String username) {
-		return todoService.findByUsername(username);
+//		return todoService.findByUsername(username);
+		return todoRepository.findByUsername(username);
 	}
 
 	@GetMapping("/users/{username}/todos/{id}")
 	public Todo retrieveTodo(@PathVariable String username, @PathVariable int id) {
-		return todoService.findById(id);
+//		return todoService.findById(id);
+		return todoRepository.findById(id).get();
 	}
 
 	@DeleteMapping("/users/{username}/todos/{id}")
 	public ResponseEntity<Void>  deleteTodo(@PathVariable String username, @PathVariable int id) {
-		todoService.deleteById(id);
+//		todoService.deleteById(id);
+		todoRepository.deleteById(id);
 		return ResponseEntity.noContent().build();
 	}
 	
 	@PutMapping("/users/{username}/todos/{id}")
 	public Todo  updateTodo(@PathVariable String username, @PathVariable int id, @RequestBody Todo todo) {
-		todoService.updateTodo(todo);
+//		todoService.updateTodo(todo);
+		todoRepository.save(todo);
 		return todo;
 	}
 
